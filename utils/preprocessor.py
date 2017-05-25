d07244d4d3fab96da7eb3e2a8d8824304ff54316
Lingkun <llkpersonal@126.com>
2017-05-25 16:54:13 +0800

    [M] 旋转代码

--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -1,5 +1,17 @@
+import cv2
+
 # TODO: SIFT ALGORITHM - Linrong Jin
 
 # TODO: SaliencyELD Call - Linrong Jin
 
 # TODO: Data Augmentation - Lingkun Li
+def rotateImage(image, angle):
+    height = image.shape[0]
+    width = image.shape[1]
+    height_big = height
+    width_big = width
+    image_big = cv2.resize(image, (width_big, height_big))
+    image_center = (width_big/2, height_big/2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image_big, rot_mat, (width_big, height_big), flags=cv2.INTER_LINEAR)
+    return result
