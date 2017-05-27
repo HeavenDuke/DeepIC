8a3e69e4ff84702d6800892749c8513c54385ad0
Lingkun <llkpersonal@126.com>
2017-05-27 11:28:23 +0800

    [A] 旋转 [A] 缩放 [A] 平移 [A] 对比度 [A] 噪点

--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # TODO: SIFT ALGORITHM - Linrong Jin
 
@@ -15,3 +16,38 @@ def rotateImage(image, angle):
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
     result = cv2.warpAffine(image_big, rot_mat, (width_big, height_big), flags=cv2.INTER_LINEAR)
     return result
+
+
+# flag = 0 / 1 / -1
+# 旋转: 垂直/ 水平 / 水平与垂直
+def flipImage(image, flag):
+    return cv2.flip(image, flag)
+
+
+def scale(image, multiple, interpolation = None):
+    rows, cols, channels = image.shape
+    return cv2.resize(image, (int(cols*multiple),int(rows*multiple)), interpolation=interpolation)
+
+
+def zoom(image, dsize, interpolation = None):
+    return cv2.resize(image, dsize, interpolation=interpolation)
+
+
+def shift(image, x, y):
+    M = np.array([[1, 0, x],[0, 1, y]], np.float32)
+    rows, cols, channels = image.shape
+    return cv2.warpAffine(image, M, (cols,rows))
+
+
+# 对比度变换
+def contrast(image, alpha, beta):
+    image = image * alpha + beta
+    return image
+
+# size - 噪点数量
+def noise(image, size):
+    for i in range(0,size):
+        xi = int(np.random.uniform(0,image.shape[1]))
+        xj = int(np.random.uniform(0,image.shape[0]))
+        image[xj,xi] = 255
+    return image
