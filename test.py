55bb1b172c1aff6b42f650dd6f5b6d9eaae7c54b
HeavenDuke <trashlhc@163.com>
2017-05-30 20:27:47 +0800

    测试

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ def imageSIFT(img, n_clusters = 100):
     keypoints, descriptors = s.detectAndCompute(pic, None)
     descriptors = normalize(descriptors, norm = 'l2', axis = 1)
     centroid, l, i = k_means(descriptors, n_clusters = n_clusters)
+    print centroid.shape
     return np.reshape(centroid, newshape = (1, n_clusters * 128))
 
 
