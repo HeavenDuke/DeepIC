5e54f15e173f6786dab3476e26fea41b0eeff294
HeavenDuke <trashlhc@163.com>
2017-05-30 19:52:00 +0800

    测试

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
+import cv2
 import numpy as np
+from sklearn.cluster import k_means
+from sklearn.preprocessing import normalize
 
 from utils.loader import construct_input_data
-from utils.preprocessor import shuffle, extractSIFT
+from utils.preprocessor import shuffle
+
+
+def imageSIFT(img, n_clusters = 100):
+    s = cv2.SIFT()
+    keypoints, descriptors = s.detectAndCompute(img, None)
+    descriptors = normalize(descriptors, norm = 'l2', axis = 1)
+    return np.reshape(k_means(descriptors, n_clusters = n_clusters), newshape = (1, n_clusters * 128))
+
+
+def extractSIFT(images):
+    return np.asarray([imageSIFT(np.reshape(images[index], newshape = (64, 64, 3))) for index in range(images.shape[0])])
+
 
 validation_split = 0.9
 
