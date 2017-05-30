225bbcb5f29243be584981f32e87e1b5d0a48737
HeavenDuke <trashlhc@163.com>
2017-05-30 22:20:02 +0800

    加上mask试试看

--- run.py
+++ run.py
@@ -13,8 +13,8 @@ validation_split = 0.9
 class_num = 12
 enhanced_class_num = 10
 
-x, y = construct_input_data('./data/train', with_masks = False)
-x_extra, y_extra = construct_input_data('./data/extra', with_masks = False)
+x, y = construct_input_data('./data/train', with_masks = True)
+x_extra, y_extra = construct_input_data('./data/extra', with_masks = True)
 
 x, y = x + x_extra, np.concatenate((y, y_extra))
 
@@ -75,7 +75,7 @@ generator = ImageDataGenerator(
 generator.fit(x_train)
 
 classifier.fit_generator(generator.flow(x_train, y_train, batch_size = 32),
-                         epochs = 100,
+                         epochs = 500,
                          verbose = 1,
                          steps_per_epoch = x_train.shape[0] // 32,
                          validation_steps = 10,
