f828742227e3cd6faad439c7d47dc2d30f16faf0
HeavenDuke <trashlhc@163.com>
2017-06-01 21:40:38 +0800

    首先测试最naive的模型

--- run.py
+++ run.py
@@ -38,15 +38,15 @@ x, y, x_sift = shuffle(x, y, None)# x_sift)
 x_train, y_train = x[:int(x.shape[0] * validation_split)], y[:int(x.shape[0] * validation_split)]
 x_test, y_test = x[int(x.shape[0] * validation_split):], y[int(x.shape[0] * validation_split):]
 
-# (x_train_p, y_train_p), (x_test_p, y_test_p) = cifar10.load_data()
-# x_train_p, x_test_p = resizeImages(x_train_p, size = (128, 128)), resizeImages(x_test_p, size = (128, 128))
-# y_train_p = to_categorical(y_train_p, 10)
-# y_test_p = to_categorical(y_test_p, 10)
-# #
-# x_train_p = x_train_p.astype('float32')
-# x_test_p = x_test_p.astype('float32')
-# x_train_p /= 255.
-# x_test_p /= 255.
+(x_train_p, y_train_p), (x_test_p, y_test_p) = cifar10.load_data()
+x_train_p, x_test_p = resizeImages(x_train_p, size = (128, 128)), resizeImages(x_test_p, size = (128, 128))
+y_train_p = to_categorical(y_train_p, 10)
+y_test_p = to_categorical(y_test_p, 10)
+#
+x_train_p = x_train_p.astype('float32')
+x_test_p = x_test_p.astype('float32')
+x_train_p /= 255.
+x_test_p /= 255.
 
 print "finish loading data"
 
@@ -57,30 +57,30 @@ print "finish loading data"
 from keras.optimizers import SGD, RMSprop
 
 classifier, classifier_p = ResnetBuilder.build_resnet_34(input_shape = (3, 128, 128), num_outputs = 12, enhanced = True)
-# classifier_p.compile(loss = "categorical_crossentropy", optimizer = SGD(lr = 1e-3, decay = 1e-3), metrics = ['accuracy'])
+classifier_p.compile(loss = "categorical_crossentropy", optimizer = SGD(lr = 1e-3, decay = 1e-3), metrics = ['accuracy'])
 classifier.compile(loss = "categorical_crossentropy", optimizer = RMSprop(lr = 5e-4, decay = 0.05), metrics = ['accuracy'])
 
-# generator = ImageDataGenerator(
-#     featurewise_center = False,  # set input mean to 0 over the dataset
-#     samplewise_center = False,  # set each sample mean to 0
-#     featurewise_std_normalization = False,  # divide inputs by std of the dataset
-#     samplewise_std_normalization = False,  # divide each input by its std
-#     zca_whitening = False,  # apply ZCA whitening
-#     rotation_range = 0,  # randomly rotate images in the range (degrees, 0 to 180)
-#     width_shift_range = 0.1,  # randomly shift images horizontally (fraction of total width)
-#     height_shift_range = 0.1,  # randomly shift images vertically (fraction of total height)
-#     horizontal_flip = True,  # randomly flip images
-#     vertical_flip = False  # randomly flip images
-# )
+generator = ImageDataGenerator(
+    featurewise_center = False,  # set input mean to 0 over the dataset
+    samplewise_center = False,  # set each sample mean to 0
+    featurewise_std_normalization = False,  # divide inputs by std of the dataset
+    samplewise_std_normalization = False,  # divide each input by its std
+    zca_whitening = False,  # apply ZCA whitening
+    rotation_range = 0,  # randomly rotate images in the range (degrees, 0 to 180)
+    width_shift_range = 0.1,  # randomly shift images horizontally (fraction of total width)
+    height_shift_range = 0.1,  # randomly shift images vertically (fraction of total height)
+    horizontal_flip = True,  # randomly flip images
+    vertical_flip = False  # randomly flip images
+)
 
-# generator.fit(x_train_p)
-#
-# classifier_p.fit_generator(generator.flow(x_train_p, y_train_p, batch_size = 128),
-#                            epochs = 100,
-#                            verbose = 1,
-#                            steps_per_epoch = x_train_p.shape[0] // 128,
-#                            validation_steps = 10,
-#                            validation_data = (x_test_p, y_test_p))
+generator.fit(x_train_p)
+
+classifier_p.fit_generator(generator.flow(x_train_p, y_train_p, batch_size = 128),
+                           epochs = 100,
+                           verbose = 1,
+                           steps_per_epoch = x_train_p.shape[0] // 128,
+                           validation_steps = 10,
+                           validation_data = (x_test_p, y_test_p))
 
 # generator.fit(x_train)
 
@@ -94,9 +94,9 @@ classifier.compile(loss = "categorical_crossentropy", optimizer = RMSprop(lr = 5
 
 classifier.fit(x, y, batch_size = 32, validation_split = 0.1, epochs = 100, shuffle = True, verbose = True)
 
-# KFold = StratifiedKFold(n_splits = 10)
-#
-# print cross_val_score(classifier, x, y, cv = KFold)
+KFold = StratifiedKFold(n_splits = 10)
+
+print cross_val_score(classifier, x, y, cv = KFold)
 
 # classifier_e.fit([x_train, x_train_sift], y_train, batch_size = 32, epochs = 100, validation_split = 0.1, verbose = True)
 
