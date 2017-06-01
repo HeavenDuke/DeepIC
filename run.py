ab84aa46f97dfbc36526bf7d98db181461e90e4c
HeavenDuke <trashlhc@163.com>
2017-06-01 20:16:52 +0800

    去掉背景

--- run.py
+++ run.py
@@ -16,8 +16,8 @@ validation_split = 0.8
 class_num = 12
 enhanced_class_num = 10
 
-x, y = construct_input_data('./data/train', with_masks = False)
-x_extra, y_extra = construct_input_data('./data/extra', with_masks = False)
+x, y = construct_input_data('./data/train', with_masks = True)
+x_extra, y_extra = construct_input_data('./data/extra', with_masks = True)
 
 x, y = x + x_extra, np.concatenate((y, y_extra))
 
