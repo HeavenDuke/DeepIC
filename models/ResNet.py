0be887d1e3d6303428e35f2a2038cbdbed21d500
HeavenDuke <trashlhc@163.com>
2017-06-01 21:19:53 +0800

    尝试添加一个全连接层

--- models/ResNet.py
+++ models/ResNet.py
@@ -23,7 +23,7 @@ from layers.SpatialPyramidPooling import SpatialPyramidPooling
 def _bn_relu(input):
     """Helper to build a BN -> relu block
     """
-    norm = BatchNormalization(axis=CHANNEL_AXIS)(input)
+    norm = BatchNormalization(axis = CHANNEL_AXIS)(input)
     return Activation("relu")(norm)
 
 
@@ -38,10 +38,10 @@ def _conv_bn_relu(**conv_params):
     kernel_regularizer = conv_params.setdefault("kernel_regularizer", l2(0.01))
 
     def f(input):
-        conv = Conv2D(filters=filters, kernel_size=kernel_size,
-                      strides=strides, padding=padding,
-                      kernel_initializer=kernel_initializer,
-                      kernel_regularizer=kernel_regularizer)(input)
+        conv = Conv2D(filters = filters, kernel_size = kernel_size,
+                      strides = strides, padding = padding,
+                      kernel_initializer = kernel_initializer,
+                      kernel_regularizer = kernel_regularizer)(input)
         return _bn_relu(conv)
 
     return f
@@ -60,10 +60,10 @@ def _bn_relu_conv(**conv_params):
 
     def f(input):
         activation = _bn_relu(input)
-        return Conv2D(filters=filters, kernel_size=kernel_size,
-                      strides=strides, padding=padding,
-                      kernel_initializer=kernel_initializer,
-                      kernel_regularizer=kernel_regularizer)(activation)
+        return Conv2D(filters = filters, kernel_size = kernel_size,
+                      strides = strides, padding = padding,
+                      kernel_initializer = kernel_initializer,
+                      kernel_regularizer = kernel_regularizer)(activation)
 
     return f
 
@@ -83,76 +83,79 @@ def _shortcut(input, residual):
     shortcut = input
     # 1 X 1 conv if shape is different. Else identity.
     if stride_width > 1 or stride_height > 1 or not equal_channels:
-        shortcut = Conv2D(filters=residual_shape[CHANNEL_AXIS],
-                          kernel_size=(1, 1),
-                          strides=(stride_width, stride_height),
-                          padding="valid",
-                          kernel_initializer="he_normal",
-                          kernel_regularizer=l2(0.01))(input)
+        shortcut = Conv2D(filters = residual_shape[CHANNEL_AXIS],
+                          kernel_size = (1, 1),
+                          strides = (stride_width, stride_height),
+                          padding = "valid",
+                          kernel_initializer = "he_normal",
+                          kernel_regularizer = l2(0.01))(input)
 
     return add([shortcut, residual])
 
 
-def _residual_block(block_function, filters, repetitions, is_first_layer=False):
+def _residual_block(block_function, filters, repetitions, is_first_layer = False):
     """Builds a residual block with repeating bottleneck blocks.
     """
+
     def f(input):
         for i in range(repetitions):
             init_strides = (1, 1)
             if i == 0 and not is_first_layer:
                 init_strides = (2, 2)
-            input = block_function(filters=filters, init_strides=init_strides,
-                                   is_first_block_of_first_layer=(is_first_layer and i == 0))(input)
+            input = block_function(filters = filters, init_strides = init_strides,
+                                   is_first_block_of_first_layer = (is_first_layer and i == 0))(input)
         return input
 
     return f
 
 
-def basic_block(filters, init_strides=(1, 1), is_first_block_of_first_layer=False):
+def basic_block(filters, init_strides = (1, 1), is_first_block_of_first_layer = False):
     """Basic 3 X 3 convolution blocks for use on resnets with layers <= 34.
     Follows improved proposed scheme in http://arxiv.org/pdf/1603.05027v2.pdf
     """
+
     def f(input):
 
         if is_first_block_of_first_layer:
             # don't repeat bn->relu since we just did bn->relu->maxpool
-            conv1 = Conv2D(filters=filters, kernel_size=(3, 3),
-                           strides=init_strides,
-                           padding="same",
-                           kernel_initializer="he_normal",
-                           kernel_regularizer=l2(0.01))(input)
+            conv1 = Conv2D(filters = filters, kernel_size = (3, 3),
+                           strides = init_strides,
+                           padding = "same",
+                           kernel_initializer = "he_normal",
+                           kernel_regularizer = l2(0.01))(input)
         else:
-            conv1 = _bn_relu_conv(filters=filters, kernel_size=(3, 3),
-                                  strides=init_strides)(input)
+            conv1 = _bn_relu_conv(filters = filters, kernel_size = (3, 3),
+                                  strides = init_strides)(input)
 
-        residual = _bn_relu_conv(filters=filters, kernel_size=(3, 3))(conv1)
+        residual = _bn_relu_conv(filters = filters, kernel_size = (3, 3))(conv1)
         return _shortcut(input, residual)
 
     return f
 
 
-def bottleneck(filters, init_strides=(1, 1), is_first_block_of_first_layer=False):
+def bottleneck(filters, init_strides = (1, 1), is_first_block_of_first_layer = False):
     """Bottleneck architecture for > 34 layer resnet.
     Follows improved proposed scheme in http://arxiv.org/pdf/1603.05027v2.pdf
 
     Returns:
         A final conv layer of filters * 4
     """
+
     def f(input):
 
         if is_first_block_of_first_layer:
             # don't repeat bn->relu since we just did bn->relu->maxpool
-            conv_1_1 = Conv2D(filters=filters, kernel_size=(1, 1),
-                              strides=init_strides,
-                              padding="same",
-                              kernel_initializer="he_normal",
-                              kernel_regularizer=l2(0.01))(input)
+            conv_1_1 = Conv2D(filters = filters, kernel_size = (1, 1),
+                              strides = init_strides,
+                              padding = "same",
+                              kernel_initializer = "he_normal",
+                              kernel_regularizer = l2(0.01))(input)
         else:
-            conv_1_1 = _bn_relu_conv(filters=filters, kernel_size=(3, 3),
-                                     strides=init_strides)(input)
+            conv_1_1 = _bn_relu_conv(filters = filters, kernel_size = (3, 3),
+                                     strides = init_strides)(input)
 
-        conv_3_3 = _bn_relu_conv(filters=filters, kernel_size=(3, 3))(conv_1_1)
-        residual = _bn_relu_conv(filters=filters * 4, kernel_size=(1, 1))(conv_3_3)
+        conv_3_3 = _bn_relu_conv(filters = filters, kernel_size = (3, 3))(conv_1_1)
+        residual = _bn_relu_conv(filters = filters * 4, kernel_size = (1, 1))(conv_3_3)
         return _shortcut(input, residual)
 
     return f
@@ -208,14 +211,14 @@ class ResnetBuilder(object):
         # Load function from str if needed.
         block_fn = _get_block(block_fn)
 
-        input = Input(shape=input_shape)
-        conv1 = _conv_bn_relu(filters=64, kernel_size=(7, 7), strides=(2, 2))(input)
-        pool1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding = "same")(conv1)
+        input = Input(shape = input_shape)
+        conv1 = _conv_bn_relu(filters = 64, kernel_size = (7, 7), strides = (2, 2))(input)
+        pool1 = MaxPooling2D(pool_size = (3, 3), strides = (2, 2), padding = "same")(conv1)
 
         block = pool1
         filters = 64
         for i, r in enumerate(repetitions):
-            block = _residual_block(block_fn, filters=filters, repetitions=r, is_first_layer=(i == 0))(block)
+            block = _residual_block(block_fn, filters = filters, repetitions = r, is_first_layer = (i == 0))(block)
             filters *= 2
 
         # Last activation
@@ -223,17 +226,20 @@ class ResnetBuilder(object):
 
         # Classifier block
         block_shape = K.int_shape(block)
-        pool2 = AveragePooling2D(pool_size=(block_shape[ROW_AXIS], block_shape[COL_AXIS]),
-                                 strides=(1, 1))(block)
+        pool2 = AveragePooling2D(pool_size = (block_shape[ROW_AXIS], block_shape[COL_AXIS]),
+                                 strides = (1, 1))(block)
         flatten1 = Flatten()(pool2)
+
+        flatten1 = Dense(units = 512, kernel_initializer = "he_normal", kernel_regularizer = l2(0.01),
+                         activation = "relu")(flatten1)
 
         # flatten1 = SpatialPyramidPooling([1, 2])(pool2)
 
         if enhanced:
-            dense = Dense(units=num_outputs, kernel_initializer="he_normal", kernel_regularizer = l2(0.01),
-                          activation="softmax")(flatten1)
+            dense = Dense(units = num_outputs, kernel_initializer = "he_normal", kernel_regularizer = l2(0.01),
+                          activation = "softmax")(flatten1)
 
-            model1 = Model(inputs=input, outputs=dense)
+            model1 = Model(inputs = input, outputs = dense)
 
             dense = Dense(units = 10, kernel_initializer = "he_normal", kernel_regularizer = l2(0.01),
                           activation = "softmax")(flatten1)
