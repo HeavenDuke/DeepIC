fef0ba00e4bcb9ff3d393c3d1ef5ad5d1d29a5aa
HeavenDuke <trashlhc@163.com>
2017-06-04 16:00:35 +0800

    测试sift

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from utils.loader import construct_input_data
+from utils.preprocessor import extractSIFT
 from utils.preprocessor import group_data_by_label
 
 validation_split = 0.8
@@ -13,10 +14,14 @@ x_extra, y_extra = construct_input_data('./data/extra', with_masks = False)
 
 x, y = x + x_extra, np.concatenate((y, y_extra))
 
-x = np.asarray([np.reshape(cv2.resize(item, (128, 128)), newshape = (128, 128, 3)) for item in x])
+x_sift = extractSIFT(x, n_clusters = 100)
 
-x, y = x.astype(np.float32), y.astype(np.float32)
+print x_sift
 
-x /= 255.
-
-
+# x = np.asarray([np.reshape(cv2.resize(item, (128, 128)), newshape = (128, 128, 3)) for item in x])
+#
+# x, y = x.astype(np.float32), y.astype(np.float32)
+#
+# x /= 255.
+#
+#
