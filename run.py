2b1409e726d1b780474fd1faa9f86b1b5680ec2e
HeavenDuke <trashlhc@163.com>
2017-06-04 21:13:44 +0800

    测试运行带sift的模型

--- run.py
+++ run.py
@@ -22,11 +22,11 @@ x_extra, y_extra = construct_input_data('./data/extra', with_masks = False)
 
 x, y = x + x_extra, np.concatenate((y, y_extra))
 
-x_sift = np.random.random(size = (x.shape[0], 100))
-
 # x_sift = extractSIFT(x, n_clusters = 100)
 
 x = np.asarray([np.reshape(cv2.resize(item, (128, 128)), newshape = (128, 128, 3)) for item in x])
+
+x_sift = np.random.random(size = (x.shape[0], 100))
 
 x, y = x.astype(np.float32), y.astype(np.float32)
 
