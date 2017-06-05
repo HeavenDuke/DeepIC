337d0bf36d2dd4cf604123b805c93f10e00b30b7
HeavenDuke <trashlhc@163.com>
2017-06-05 12:27:04 +0800

    测试载入模型

--- utils/writer.py
+++ utils/writer.py
@@ -1,6 +1,6 @@
 
 
 def save_prediction(ids, prediction, filename = None):
-    lines = ["%s\t%d" % (ids[i], prediction[0, i]) for i in range(len(ids))]
+    lines = ["%s\t%d" % (ids[i], prediction[i]) for i in range(len(ids))]
     with open(filename, 'w') as f:
         f.writelines(lines)
